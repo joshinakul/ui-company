b7369d58219ed5f728d45939ac9e99954912ac6b
Nakul Joshi <joshinakul1213@gmail.com>
2025-07-19 01:47:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/solutions" element={<Solutions />} />
+          {/* <Route path="/solutions" element={<Solutions />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
